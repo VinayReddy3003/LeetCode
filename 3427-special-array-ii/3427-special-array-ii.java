9ddc650cb7367340e7a732d1b8860844436a2135
Vinay Reddy <158543688+VinayReddy3003@users.noreply.github.com>
2024-12-09 23:46:07 +0530

    Time: 2 ms (100%), Space: 98.7 MB (44.79%) - LeetHub

---
+++ 3427-special-array-ii/3427-special-array-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int[] prefixSum = new int[n];
+        prefixSum[0]= 1;
+        for(int i = 1;i<n;i++) {
+            if((nums[i]&1) == (nums[i-1]&1)) {
+                prefixSum[i] = 1;
+            }
+            else {
+                prefixSum[i] = 1+ prefixSum[i-1];
+            }
+        }
+        boolean[] ans = new boolean[queries.length];
+        for(int i = 0;i<queries.length;i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            int size = end-start+1;
+
+            if(prefixSum[end]>= size) {
+                ans[i]=true;
+            }
+            else {
+                ans[i]=false;
+            }
+        }
+        return ans;
+
+    }
+}
