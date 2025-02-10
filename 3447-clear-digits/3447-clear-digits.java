b43077c350ac6bcbf933912608e8814e5bf4bbcc
Vinay Reddy <158543688+VinayReddy3003@users.noreply.github.com>
2025-02-10 09:09:41 +0530

    Time: 1 ms (100%), Space: 42.7 MB (45.64%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String clearDigits(String s) {
+        StringBuffer sb = new StringBuffer();
+        for(char c: s.toCharArray()) {
+            if(c >='0' && c<='9') {
+                 if(sb.length() >0) {
+                    sb.deleteCharAt(sb.length()-1);
+                 }
+            }
+            else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
