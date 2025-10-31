d794e961fdf622cabbd6039262e67923aa4cbd65
Vinay Reddy <158543688+VinayReddy3003@users.noreply.github.com>
2025-10-31 22:05:39 +0530

    Time: 2 ms (63.09%), Space: 46.5 MB (7.71%) - LeetHub

---
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        int[] result = new int[2];
+        int index = 0;
+
+        for(int num : nums) {
+            if(!seen.add(num)) {
+                result[index++] = num;
+            }
+        }
+
+        return result;
+
+        
+    }
+}
